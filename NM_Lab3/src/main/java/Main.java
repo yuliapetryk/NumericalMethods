d535c28d7aa92e7efac60b729d2f5a3f9f3e32ac
Yulia Petryk <yulya.petruk.03@gmail.com>
2023-11-05 12:34:52 +0200

    added a transition from the adjacency matrix to the transition matrix

--- NM_Lab3/src/main/java/Main.java
+++ NM_Lab3/src/main/java/Main.java
@@ -3,48 +3,61 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         String filePath = "\\Users\\User\\Desktop\\University\\PI-33\\Sem1\\NM\\NM_Lab3\\src\\main\\java\\graph.txt";
         double[][] matrix = readMatrixFromFile(filePath);
+        printMatrix(matrix);
 
-        //printMatrix(matrix);
-
+        double[][] transition_matrix= findTransitionMatrix(matrix);
         System.out.println();
+        printMatrix(transition_matrix);
 
-        if (!checkColumnSum(matrix)) {
+        if (!checkColumnSum(transition_matrix)) {
             System.out.println("\n" +"The sum of elements in each column must be equal to 1!");
         } else {
             double eps = 0.00001;
-            double[] vector= new double[matrix.length];
-            for (int i = 0; i < matrix.length; i++) {
-                vector[i] = 1.0;
-            }
 
-            double[] eigenVector =  findEigenVector(matrix , eps);
-            double maxEigenValue = findMaxEigenValue(matrix , eps);
+            double[] eigenVector =  findEigenVector(transition_matrix , eps);
+            double maxEigenValue = findMaxEigenValue(transition_matrix , eps);
 
-            System.out.println("Max eigen value: " + maxEigenValue); //0,99
+            System.out.println("Max eigen value: " + maxEigenValue);
             System.out.println();
             System.out.println("Result(vector of vertex ranks): ");
             printVector(eigenVector);
-            /*
-            1.6
-            1,2
-            0,3
-            0,9
-            * */
         }
     }
 
-    public static double[] findEigenVector(double[][] matrix,  double eps) throws Exception {
+    public static double[][] findTransitionMatrix(double[][] matrix) {
+
+        double[][] transition_matrix = new double[matrix.length][matrix[0].length];
+        double[] sum_array = new double[matrix[0].length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                sum_array[j] += matrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                transition_matrix[i][j] = matrix[i][j] / sum_array[j];
+            }
+        }
+
+        return transition_matrix;
+    }
+
+    public static double[] findEigenVector(double[][] matrix,  double eps)  {
         double[] vector= new double[matrix.length];
         for (int i = 0; i < matrix.length; i++) {
             vector[i] = 1.0;
         }
+
         int i=1;
         while (true) {
             double[] nextVector = matrixMultiplication(matrix, vector);
             if (normDifference(nextVector, vector) < eps) {
+                System.out.println();
                 System.out.println("Accuracy is achieved in " + i + "  iterations");
                 System.out.println();
                 return nextVector;
@@ -54,12 +67,19 @@ public class Main {
         }
     }
 
+    public static double normDifference(double[] nextVector, double[] vector) {
+        double sum = 0.0;
+        for (int i = 0; i < nextVector.length; i++) {
+            sum += Math.pow(nextVector[i] - vector[i], 2);
+        }
+        return Math.sqrt(sum);
+    }
+
     public static double findMaxEigenValue(double[][] matrix, double eps) {
         double[] vector = new double[matrix.length];
         for (int i = 0; i < matrix.length; i++) {
             vector[i] = 1;
         }
-
         double maxEigenValue = 0;
 
         while (true) {
@@ -81,14 +101,6 @@ public class Main {
             }
         }
         return result;
-    }
-
-    public static double normDifference(double[] nextVector, double[] vector) {
-        double sum = 0.0;
-        for (int i = 0; i < nextVector.length; i++) {
-            sum += Math.pow(nextVector[i] - vector[i], 2);
-        }
-        return Math.sqrt(sum);
     }
 
     public static boolean checkColumnSum(double[][] matrix) {
